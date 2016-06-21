c81aea9c3a956ab1779079f430d663eabb886869
nguyenminhtiend <nguyenminhtiend@gmail.com>
2016-06-21 22:18:30 +0700

    #add server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var open = require('open');
+var path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join( __dirname, './index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open(`http://localhost:${port}`);
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 var path = require('path');
-var webpack = require('webpack');
 
 var APP_DIR = path.resolve(__dirname, 'app');
 
 module.exports = {
+    devtool: 'cheap-module-source-map',
     entry: APP_DIR + '/main.js',
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js',
-        publicPath: 'http://localhost:63342/React-Redux/build/'
+        publicPath: 'http://localhost:3000/build/'
     },
     module: {
         loaders: [
             {
                 test: /.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                loaders: ['babel']
             },
-            {test: /\.css$/, loader: "style!css!"},
-            { test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/, loader: 'url?limit=10000' },
-            { test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/, loader: 'file' }
+            {
+                test: /\.css$/,
+                loader: "style!css!"
+            },
+            {
+                test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
+                loader: 'url?limit=10000'
+            },
+            {
+                test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
+                loader: 'file'
+            }
         ]
     }
 };
